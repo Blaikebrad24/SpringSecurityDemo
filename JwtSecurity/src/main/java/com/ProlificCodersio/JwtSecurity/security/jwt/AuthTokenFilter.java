d767e705205abf11175f873e558ef7bbeb7be3a1
Blaike Bradford <bb494v@att.com>
2022-12-27 11:16:58 -0600

    Decode argument cannot be null -> after adding methods to JwtUtils Class: Runtime error when testing /api/test/user

--- JwtSecurity/src/main/java/com/ProlificCodersio/JwtSecurity/security/jwt/AuthTokenFilter.java
+++ JwtSecurity/src/main/java/com/ProlificCodersio/JwtSecurity/security/jwt/AuthTokenFilter.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -19,6 +20,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
 
@@ -51,15 +53,30 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String username = null;
+        String authToken = parseJwt(request);
+
         try {
-            String jwt = parseJwt(request);
-            if(jwt != null && jwtUtils.validateJwtToken(jwt))
+
+            username = jwtUtils.getUserNameFromJwtToken(authToken);
+
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null)
             {
-                String username = jwtUtils.getUserNameFromJwtToken(jwt);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                if(jwtUtils.validateJwtToken(authToken, userDetails))
+                {
+                    String role ="";
+                    role = userDetails.getAuthorities().size() > 1 ? "ROLE_ADMIN":"ROLE_USER";
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                            userDetails,null, Arrays.asList(new SimpleGrantedAuthority(role)));
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    logger.info("authenticated user " + username + ", setting security context");
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+//                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+//                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+//                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         }catch(Exception e)
         {
--- JwtSecurity/src/main/java/com/ProlificCodersio/JwtSecurity/security/jwt/JwtUtils.java
+++ JwtSecurity/src/main/java/com/ProlificCodersio/JwtSecurity/security/jwt/JwtUtils.java
@@ -2,21 +2,20 @@ package com.ProlificCodersio.JwtSecurity.security.jwt;
 
 import com.ProlificCodersio.JwtSecurity.security.services.UserDetailsImpl;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.impl.DefaultClock;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.SecretKey;
 import java.security.Key;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
     Class - JwtUtils
@@ -29,32 +28,93 @@ import java.util.Date;
 public class JwtUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
-
+    private Clock clock = DefaultClock.INSTANCE;
 
     @Value("${prolific.app.jwtSecret}")
-    private String jwtSecret;
+    private static String jwtSecret;
 
     @Value("${prolific.app.jwtExpirationMs}")
-    private int jwtExpriationMs;
+    private Long jwtExpriationMs;
 
 
-    private Key getSigningKey() {
+    private JwtUtils(){}
+
+
+    private static Key getSigningKey() {
 
 //        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        byte[] keyBytes = Decoders.BASE64.decode(this.jwtSecret);
+        byte[] keyBytes = Decoders.BASE64.decode(jwtSecret);
 //        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
 
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateJwtToken(Authentication authentication)
+    public String createToken(Authentication authentication)
     {
         UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
 
         return Jwts.builder().setSubject((userPrincipal.getUsername()))
-                .setIssuedAt(new Date()).setExpiration(new Date((new Date()).getTime() + jwtExpriationMs))
+                .setIssuedAt(new Date()).setExpiration(new Date((new Date()).getTime() + 120000))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public String generateToken(UserDetails userDetails)
+    {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+    private String doGenerateToken(Map<String, Object> claims, String subject)
+    {
+        final Date createdDate = clock.now();
+        final Date expirationDate = calculateExpirationDate(createdDate);
+
+        return Jwts.builder().setClaims(claims).setSubject(subject)
+                .setIssuedAt(createdDate).setExpiration(expirationDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+    private Date calculateExpirationDate(Date createdDate)
+    {
+        return new Date(createdDate.getTime() + jwtExpriationMs * 1000);
+    }
+
+    private static Claims extractClaims(String token)
+    {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+
+    public Date getIssuedAtDateFromToken(String token)
+    {
+        return extractClaims(token).getIssuedAt();
+    }
+
+    public static String getSubject(String token)
+    {
+        Claims claims = extractClaims(token);
+        return claims.getSubject();
+    }
+    public static String refreshToken(String token, long expirationInSeconds)
+    {
+        final Claims claims = extractClaims(token);
+        Date now = new Date();
+        claims.setIssuedAt(now);
+        claims.setExpiration(new Date(now.getTime() + 120000));
+
+        return createTokenFromClaims(claims);
+    }
+
+    public static boolean isTokenExpired(String token)
+    {
+        final Claims claims = extractClaims(token);
+        Date now = new Date();
+        return now.after(claims.getExpiration());
+    }
+
+    private static String createTokenFromClaims(Claims claims)
+    {
+        return Jwts.builder().setClaims(claims).signWith(getSigningKey(),SignatureAlgorithm.HS256).compact();
     }
 
     public String getUserNameFromJwtToken(String token)
@@ -62,21 +122,25 @@ public class JwtUtils {
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody().getSubject();
     }
 
-    public boolean validateJwtToken(String authToken)
+    public boolean validateJwtToken(String authToken, UserDetails userDetails)
     {
-        try{
-            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(authToken);
-            return true;
-        }catch (MalformedJwtException e) {
-            logger.error("Invalid JWT token: {}", e.getMessage());
-        } catch (ExpiredJwtException e) {
-            logger.error("JWT token is expired: {}", e.getMessage());
-        } catch (UnsupportedJwtException e) {
-            logger.error("JWT token is unsupported: {}", e.getMessage());
-        } catch (IllegalArgumentException e) {
-            logger.error("JWT claims string is empty: {}", e.getMessage());
-        }
-        return false;
+
+        UserDetailsImpl userDetails1 = (UserDetailsImpl) userDetails;
+        final String username = getUserNameFromJwtToken(authToken);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(authToken));
+//        try{
+//            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(authToken);
+//            return true;
+//        }catch (MalformedJwtException e) {
+//            logger.error("Invalid JWT token: {}", e.getMessage());
+//        } catch (ExpiredJwtException e) {
+//            logger.error("JWT token is expired: {}", e.getMessage());
+//        } catch (UnsupportedJwtException e) {
+//            logger.error("JWT token is unsupported: {}", e.getMessage());
+//        } catch (IllegalArgumentException e) {
+//            logger.error("JWT claims string is empty: {}", e.getMessage());
+//        }
+//        return false;
     }
 
 
